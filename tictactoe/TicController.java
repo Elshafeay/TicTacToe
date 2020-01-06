11cb3ed21faef9d38c52ee1d368395cb18cce2a2
nahlaahmed97 <nahla.elaskry97@gmail.com>
2020-01-06 22:21:12 +0200

    Update TicController.java

--- tictactoe/TicController.java
+++ tictactoe/TicController.java
@@ -28,8 +28,6 @@ public class TicController implements Initializable {
     @FXML private Pane p1,p2,p3,p4,p5,p6,p7,p8,p9;
     @FXML private ImageView im1,im2,im3;
     
-    @FXML
-    private Button btn;
     
 //Fxml Action pane 1 cell 1
     @FXML
