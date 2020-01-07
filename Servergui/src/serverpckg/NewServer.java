04a6b7b37b3050188fb463606c3b8d9f236043a3
Rehab Ayman <rehabayman.fcis@gmail.com>
2020-01-07 19:00:47 +0200

    Modified NewServer

--- Servergui/src/serverpckg/NewServer.java
+++ Servergui/src/serverpckg/NewServer.java
@@ -6,35 +6,34 @@ import java.net.Socket;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.util.Pair;
 import Player.Player;
 import DBManager.DBManager;
 import GameClass.Game;
 import java.io.DataInputStream;
 import java.io.PrintStream;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class NewServer {
-    
+
     DBManager dBManager;
     //carries sokects and their username
-    public static Vector<Pair<Socket, String>> activePlayersSockets = new Vector<>();
+    public static Map<Socket, String> activePlayersSockets = new HashMap<Socket, String>();
     public static Vector<String> offlinePlayers = new Vector<>();
     public static Vector<String> onlinePlayers = new Vector<>();
-    
+
     ServerSocket serverSocket;
     volatile boolean runServer = true;
-    
-    public NewServer()
-    {
+
+    public NewServer() {
         try {
             serverSocket = new ServerSocket(5005);
             dBManager = new DBManager();
             offlinePlayers = DBManager.getPlayersIndexes();
-            while (runServer) 
-            {                
+            while (runServer) {
                 Socket s = serverSocket.accept();
                 new ConnectionHandler(s);
             }
@@ -43,24 +42,23 @@ public class NewServer {
             Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void closeServer()
-    {
+
+    public void closeServer() {
         //Not Yet fully Implemented
         runServer = false;
     }
-    
+
     //inner class for connection handling
-    class ConnectionHandler extends Thread
-    {
+    class ConnectionHandler extends Thread {
+
         private Socket clientSocket;
+        private Socket secondPlayerSocket;
         private DataInputStream clientDataInputStream;
         private PrintStream clientPrintStream;
         private JSONObject Rjson; //for receiving
         private JSONObject Sjson; //for sending
-        
-        public ConnectionHandler(Socket s)
-        {
+
+        public ConnectionHandler(Socket s) {
             try {
                 clientSocket = s;
                 clientDataInputStream = new DataInputStream(s.getInputStream());
@@ -75,17 +73,17 @@ public class NewServer {
         @Override
         public void run() {
             super.run();
-            while(true){
+            while (true) {
                 try {
                     Rjson = new JSONObject(clientDataInputStream.readLine());
-                    switch(Rjson.getString("code")){
+                    switch (Rjson.getString("code")) {
                         case "LOGIN":
                             Sjson = new JSONObject();
                             Sjson.put("code", "LOGIN");
-                            if(acceptLogin(Rjson.getString("username"),
-                                        Rjson.getString("username"))){
+                            if (acceptLogin(Rjson.getString("username"),
+                                    Rjson.getString("username"))) {
                                 Sjson.put("response", 1); //successful login
-                            }else{
+                            } else {
                                 Sjson.put("response", 0); //unsuccessful login
                             }
                             clientPrintStream.print(Sjson);
@@ -93,13 +91,13 @@ public class NewServer {
                         case "SIGNUP":
                             Sjson = new JSONObject();
                             Sjson.put("code", "SIGNUP");
-                            if(acceptSignUp(new Player(Rjson.getString("firstname"),
+                            if (acceptSignUp(new Player(Rjson.getString("firstname"),
                                     Rjson.getString("lastname"),
                                     Rjson.getString("username"),
                                     Rjson.getString("password"))
-                                    )){
+                            )) {
                                 Sjson.put("response", 1); //successful signup
-                            }else{
+                            } else {
                                 Sjson.put("response", 0); //unsuccessful signup
                             }
                             clientPrintStream.print(Sjson);
@@ -107,22 +105,22 @@ public class NewServer {
                         case "LOGOUT":
                             Sjson = new JSONObject();
                             Sjson.put("code", "LOGOUT");
-                            if(acceptLogOut()){
+                            if (acceptLogOut()) {
                                 Sjson.put("response", 1); //successful signup
-                            }else{
+                            } else {
                                 Sjson.put("response", 0); //unsuccessful signup
                             }
                             clientPrintStream.print(Sjson);
                             break;
                         case "INVITATION":
-                            switch(Rjson.getString("type")){
+                            switch (Rjson.getString("type")) {
                                 case "SEND":
                                     Sjson = new JSONObject();
                                     Sjson.put("code", "INVITATION");
                                     Sjson.put("type", "SEND");
-                                    if(sendInvitation(Rjson.getString("username"))){
+                                    if (sendInvitation(Rjson.getString("username"))) {
                                         Sjson.put("response", 1); //successful sending
-                                    }else{
+                                    } else {
                                         Sjson.put("response", 0); //unsuccessful sending
                                     }
                                     clientPrintStream.print(Sjson);
@@ -144,39 +142,36 @@ public class NewServer {
                         case "CLOSING":
                             break;
                     }
-                } catch (IOException ex) {
+                } catch (IOException | JSONException ex) {
                     Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
-        
-        public boolean acceptLogin(String username, String password)
-        {
+
+        public boolean acceptLogin(String username, String password) {
             boolean authenticationResponse = false;
             Player pTemp;
-            
+
             try {
                 pTemp = dBManager.getPlayer(username);
             } catch (SQLException ex) {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
                 return false;
             }
-            
-            if(pTemp != null && pTemp.getPass().equals(password))
-            {
-                activePlayersSockets.add(new Pair<Socket, String>(clientSocket, username));
+
+            if (pTemp != null && pTemp.getPass().equals(password)) {
+                activePlayersSockets.put(clientSocket, username);
                 offlinePlayers.remove(username);
                 onlinePlayers.add(username);
                 authenticationResponse = true;
             }
-            
+
             return authenticationResponse;
         }
-        
-        public boolean acceptSignUp(Player p)
-        {
+
+        public boolean acceptSignUp(Player p) {
             boolean signUpResponse;
-            
+
             try {
                 dBManager.addPlayer(p);
                 signUpResponse = true;
@@ -184,16 +179,15 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
                 return false;
             }
-            
+
             return signUpResponse;
         }
-        
-        public boolean acceptLogOut()
-        {
+
+        public boolean acceptLogOut() {
             boolean logOutResponse = true;
             try {
                 //get the username of the socket
-                String username = activePlayersSockets.get(activePlayersSockets.indexOf(clientSocket)).getValue();
+                String username = activePlayersSockets.get(clientSocket);
                 onlinePlayers.remove(username);
                 offlinePlayers.add(username);
                 //should close the stream of the client
@@ -207,19 +201,20 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
                 return false;
             }
-            
+
             return logOutResponse;
         }
-        
-        public boolean sendInvitation(String p2Username)
-        {
+
+        public boolean sendInvitation(String p2Username) {
             boolean invitationResponse;
-            
-            String invitationMessage = activePlayersSockets.get(activePlayersSockets.indexOf(clientSocket)).getValue()
+
+            String invitationMessage = activePlayersSockets.get(clientSocket)
                     + " has invited you to play. What do you think?";
-            
-            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
-            
+
+            if(secondPlayerSocket == null)
+                secondPlayerSocket = searchSecondSocket(p2Username);
+//            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
+
             PrintStream player2PS;
             DataInputStream player2DIS;
             //invitation should be sent and get response to set it to invitationResponse
@@ -230,23 +225,22 @@ public class NewServer {
                 invitationObject.put("type", "RECEIVE");
                 invitationObject.put("message", invitationMessage);
                 player2PS.print(invitationObject.toString());
-                
+
                 player2DIS = new DataInputStream(secondPlayerSocket.getInputStream());
                 invitationResponse = player2DIS.readBoolean();
-                
+
                 player2DIS.close();
                 player2PS.close();
-                
+
             } catch (IOException | JSONException ex) {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
                 return false;
             }
-            
+
             return invitationResponse;
         }
-        
-        public void sendRejection(String p2Username)
-        {
+
+        public void sendRejection(String p2Username) {
             String message = p2Username + " has rejected your invitation.";
             JSONObject invitationRejObj = new JSONObject();
             try {
@@ -257,9 +251,8 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        public void sendAcceptance(String p2Username)
-        {
+
+        public void sendAcceptance(String p2Username) {
             String message = p2Username + " has accepted your invitation.";
             JSONObject invitationAccObj = new JSONObject();
             try {
@@ -270,11 +263,12 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        public void sendMove(String username, int index)
-        {
+
+        public void sendMove(String username, int index) {
             //get the socket of the username and send the index to it
-            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(username)).getKey();
+            if(secondPlayerSocket == null)
+                secondPlayerSocket = searchSecondSocket(username);
+//            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(username)).getKey();
             JSONObject moveObj = new JSONObject();
             try {
                 moveObj.append("type", "Move");
@@ -286,18 +280,16 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        public void setPlayerPoints(String winnerUsername)
-        {
+
+        public void setPlayerPoints(String winnerUsername) {
             try {
                 dBManager.addingBouns(winnerUsername);
             } catch (SQLException ex) {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        public void saveGame(String p1Username, String p2Username, String board)
-        {
+
+        public void saveGame(String p1Username, String p2Username, String board) {
             //use the database function to save the game
             try {
                 dBManager.addGame(new Game(p1Username, p2Username, board));
@@ -305,21 +297,20 @@ public class NewServer {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        public String resumeGame(int gameID) throws SQLException
-        {
+
+        public String resumeGame(int gameID) throws SQLException {
             return dBManager.getGame(gameID).getBoard();
         }
-        
+
         //public JSONObject getPlayers(){}
-        
-        public void informClosing(String p2Username)
-        {
-            String closingMessage = activePlayersSockets.get(activePlayersSockets.indexOf(clientSocket)).getValue()
+        public void informClosing(String p2Username) {
+            String closingMessage = activePlayersSockets.get(clientSocket)
                     + " has closed the game.";
-            
-            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
-            
+
+            if(secondPlayerSocket == null)
+                secondPlayerSocket = searchSecondSocket(p2Username);
+//            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
+
             JSONObject closingObj = new JSONObject();
             try {
                 closingObj.append("type", "Close");
@@ -330,15 +321,16 @@ public class NewServer {
             } catch (JSONException | IOException ex) {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
         }
-        
-        public void informSaving(String p2Username)
-        {
-        String savingMessage = activePlayersSockets.get(activePlayersSockets.indexOf(clientSocket)).getValue()
+
+        public void informSaving(String p2Username) {
+            String savingMessage = activePlayersSockets.get(clientSocket)
                     + " has saved the game.";
 
-            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
+            if(secondPlayerSocket == null)
+                secondPlayerSocket = searchSecondSocket(p2Username);
+//            Socket secondPlayerSocket = activePlayersSockets.get(activePlayersSockets.indexOf(p2Username)).getKey();
 
             JSONObject savingObj = new JSONObject();
             try {
@@ -349,7 +341,19 @@ public class NewServer {
                 player2PS.close();
             } catch (JSONException | IOException ex) {
                 Logger.getLogger(NewServer.class.getName()).log(Level.SEVERE, null, ex);
-            }   
+            }
+        }
+
+        Socket searchSecondSocket(String p2Username) {
+
+            Socket secondPlayerSocket = new Socket();
+            for (Map.Entry<Socket, String> item : activePlayersSockets.entrySet()) {
+                if (item.getValue().equals(p2Username)) {
+                    secondPlayerSocket = item.getKey();
+                    break;
+                }
+            }
+            return secondPlayerSocket;
         }
     }
 }
