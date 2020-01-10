ab334d262ace449a90c959402edaa167ceaec918
nahlaahmed97 <nahla.elaskry97@gmail.com>
2020-01-10 11:17:02 +0200

    updates in serverGui fxml

--- Servergui/src/test/FXMLDocumentController.java
+++ Servergui/src/test/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.net.URL;
@@ -15,10 +10,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author nahla ahmed
- */
+
+
 public class FXMLDocumentController implements Initializable {
     @FXML
     Button stop;
@@ -33,7 +26,7 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
